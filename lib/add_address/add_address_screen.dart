0e0b23c2ededf7c9628e5e34ec8defac35b0bb7a
suren-grigoryan-sociomantic <41911541+suren-grigoryan-sociomantic@users.noreply.github.com>
2018-08-29 17:25:11 +0200

    Added AddAddressScreen widget test (#3)
    * New add address screen, code cleanup, nothing serious!
    
    * Added city, street input fields, nothing serious
    
    * Added logging middlware, nothing serious
    
    * Added test for reducers, nothing serious!
    
    * Widget test for AddAddressScreen, nothing serious

--- lib/add_address/add_address_screen.dart
+++ lib/add_address/add_address_screen.dart
@@ -15,6 +15,9 @@ class AddAddressScreen extends StatelessWidget {
   static final GlobalKey<FormFieldState<String>> _streetKey =
       GlobalKey<FormFieldState<String>>();
 
+  static GlobalKey<FormFieldState<String>> get cityKey => _cityKey;
+  static GlobalKey<FormFieldState<String>> get streetKey => _streetKey;
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, AddAddressViewModel>(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,70 @@
 // are correct.
 
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:address_book/main.dart';
+import 'package:address_book/add_address/add_address_screen.dart';
+import 'package:address_book/reducers.dart';
+import 'package:address_book/app_state.dart';
+import 'package:address_book/address.dart';
+
+class AddressBookApp extends StatelessWidget {
+  final Store<AppState> store = Store(
+    appStateReducer,
+    initialState: AppState.initialState(),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    Widget addAddress = StoreBuilder<AppState>(
+        onInit: (Store<AppState> store) {},
+        builder: (context, store) => AddAddressScreen());
+
+    MaterialApp child =
+        MaterialApp(title: 'Hello', routes: {'/': (context) => addAddress});
+    return StoreProvider(store: store, child: child);
+  }
+}
+
+final cityValidatorMsg = 'City is empty';
+final streetValidatorMsg = 'Street is empty';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new AddressBookApp('Berlin', 'Paul Linke ufer', 39));
+  group('widget test', () {
+    testWidgets('Adds a new address to address list and changes the State',
+        (WidgetTester tester) async {
+      final app = AddressBookApp();
+      await tester.pumpWidget(app);
 
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
+      final city = 'Yerevan';
+      final street = 'Abovyan';
 
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+      await tester.enterText(find.byKey(AddAddressScreen.cityKey), city);
+      await tester.enterText(find.byKey(AddAddressScreen.streetKey), street);
 
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+      await tester.tap(find.byType(RaisedButton));
+      await tester.pump();
+
+      expect(find.text(streetValidatorMsg), findsNothing);
+      expect(find.text(cityValidatorMsg), findsNothing);
+
+      AppState expectedState = AppState.initialState();
+      expectedState.placemarks.add(Address(city: city, street: street));
+
+      expect(app.store.state, expectedState);
+    });
+
+    testWidgets('City and street validator test', (WidgetTester tester) async {
+      await tester.pumpWidget(AddressBookApp());
+
+      await tester.tap(find.byType(RaisedButton));
+
+      await tester.pump();
+      expect(find.text(cityValidatorMsg), findsOneWidget);
+      expect(find.text(streetValidatorMsg), findsOneWidget);
+    });
   });
 }

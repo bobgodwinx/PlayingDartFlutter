4d354c8aca1f4bd301413a22fe65ccbd476b0365
Suren Grigoryan <suren.grigoryan@gmail.com>
2018-09-03 17:40:56 +0200

    Initial introduction of `AddressProvider` which conforms to `AddressProviderType`
    - Dropped `address_repository.dart` which is no longer needed
    - Renaming `repositories` to `providers`

--- lib/providers/address_provider.dart
+++ lib/providers/address_provider.dart
@@ -1,19 +1,21 @@
 import 'dart:async';
 
 import '../address.dart';
+import 'address_provider_type.dart';
 
-abstract class AddressRepository {
-  Future<List<Address>> load();
-  Future<bool> save();
-}
-
-class AddressRepositoryImpl implements AddressRepository {
+class AddressProvider implements AddressProviderType {
   List<Address> addressList = [
     Address(city: 'Berlin', street: 'Walkenstrasse', number: 10),
     Address(city: 'Yerevan', street: 'Koryun', number: 7),
     Address(city: 'Berlin', street: 'paul-lincke-uffer', number: 39),
   ];
+
+  AddressProvider() {}
+
+  factory AddressProvider.instance() => AddressProvider();
+
   Future<List<Address>> load() async {
+    // throw Exception('an error!');
     return Future.delayed(Duration(seconds: 2), () => addressList);
   }
 

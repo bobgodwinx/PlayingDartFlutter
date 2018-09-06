274a3874e8cf5d75c7a418569d594da01f4ce871
Bob Godwin <bobgodwinx@gmail.com>
2018-09-06 19:00:02 +0200

    Cleanup implementation for `MiddlewareMaager`

---
+++ lib/middleware_manager.dart
@@ -0,0 +1,23 @@
+import 'package:redux/redux.dart';
+import 'package:address_book/address_controller.dart';
+import 'app_state.dart';
+import 'actions.dart';
+
+abstract class StateMiddleware {
+  Middleware<AppState> addressMiddleware;
+  Middleware<AppState> loggingMiddleware;
+
+  List<Middleware> middlewares();
+}
+
+class MiddlewareManager implements StateMiddleware {
+  Middleware<AppState> addressMiddleware;
+  Middleware<AppState> loggingMiddleware;
+
+  MiddlewareManager(AddressControllerType controller) {
+    addressMiddleware = TypedMiddleware<AppState, LoadAddressesAction>(controller.loadAddresses);
+    loggingMiddleware = TypedMiddleware<AppState, dynamic>(controller.logging);
+  }
+
+  List<Middleware> middlewares() => [addressMiddleware, loggingMiddleware];
+}

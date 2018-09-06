b0279205a5e003a94aaae6c401f021df2fe03d0c
Suren Grigoryan <suren.grigoryan@gmail.com>
2018-09-06 17:06:48 +0200

    Introduced new `AddressController` class
    - Modified `MiddlewareManager` class to reflect changes coming from newly introduced `AddressController` class

--- lib/midlwares.dart
+++ lib/midlwares.dart
@@ -11,39 +11,48 @@ abstract class StateMiddleware {
   List<Middleware> middlewares();
 }
 
+class AddressController {
+  AddressProviderType addressProvider;
+  AddressController(this.addressProvider);
+
+  /// Loads address list.
+  /// Dispatches an action IsLoadingAction when starts loading.
+  /// When loading is finished IsLoadingAction and LoadedAddressesAction are dispatched.
+  loadAddresses(
+      Store<AppState> store, LoadAddressesAction action, NextDispatcher next) {
+    addressProvider.load().then((addressList) {
+      /// upon arrival of `addressList` we then use
+      /// `store` to dispatch our next actions
+      store.dispatch(LoadedAddressesAction(addressList));
+      store.dispatch(IsLoadingAction(false));
+    }).catchError((e) {
+      /// In case of Error we handle it here.
+      store.dispatch(IsLoadingAction(false));
+    });
+
+    /// This is the immediate action
+    /// when this closure is called
+    next(IsLoadingAction(true));
+  }
+
+  // todo move to it's proper class later
+  /// Logs state and an action
+  logging(Store<AppState> store, dynamic action, NextDispatcher next) {
+    print(store.state);
+    print('action: $action');
+
+    next(action);
+  }
+}
+
 class MiddlewareManager implements StateMiddleware {
   Middleware<AppState> addressMiddleware;
   Middleware<AppState> loggingMiddleware;
 
-  MiddlewareManager({AddressProviderType addressProvider}) {
-    /// Loads address list.
-    /// Dispatches an action IsLoadingAction when starts loading.
-    /// When loading is finished IsLoadingAction and LoadedAddressesAction are dispatched.
-    loadAddresses(Store<AppState> store, LoadAddressesAction action, NextDispatcher next) {
-      addressProvider.load().then((addressList) {
-        /// upon arrival of `addressList` we then use
-        /// `store` to dispatch our next actions 
-        store.dispatch(LoadedAddressesAction(addressList));
-        store.dispatch(IsLoadingAction(false));
-      }).catchError((e) {
-        /// In case of Error we handle it here.
-        store.dispatch(IsLoadingAction(false));
-      });
-      /// This is the immediate action 
-      /// when this closure is called
-      next(IsLoadingAction(true));
-    }
-
-    /// Logs state and an action
-    logging(Store<AppState> store, dynamic action, NextDispatcher next) {
-      print(store.state);
-      print('action: $action');
-
-      next(action);
-    }
-
-    addressMiddleware = TypedMiddleware<AppState, LoadAddressesAction>(loadAddresses);
-    loggingMiddleware = TypedMiddleware<AppState, dynamic>(logging);
+  MiddlewareManager(AddressController controller) {
+    addressMiddleware =
+        TypedMiddleware<AppState, LoadAddressesAction>(controller.loadAddresses);
+    loggingMiddleware = TypedMiddleware<AppState, dynamic>(controller.logging);
   }
 
   List<Middleware> middlewares() => [addressMiddleware, loggingMiddleware];

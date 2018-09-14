16ee68d55dbeb5f5d13c67e5c29dd9361a694691
Suren Grigoryan <suren.grigoryan@gmail.com>
2018-09-14 11:50:45 +0200

    AddressProvider: use `FirebaseRpository` to handle the data

--- lib/providers/address_provider.dart
+++ lib/providers/address_provider.dart
@@ -1,29 +1,25 @@
 import 'dart:async';
 
 import '../address.dart';
-import '../PreferencesRepository.dart';
+import '../repositories/firebase_repository.dart';
+import '../repositories/repository.dart';
+
 import 'address_provider_type.dart';
 
 class AddressProvider implements AddressProviderType {
-  List<Address> addressList = [
-    Address(city: 'Berlin', street: 'Walkenstrasse', number: 10),
-    Address(city: 'Yerevan', street: 'Koryun', number: 7),
-    Address(city: 'Berlin', street: 'paul-lincke-uffer', number: 39),
-  ];
+  
+//todo use dependency injection?
+  Repository _repositry = FirebaseRepository();
 
-  AddressProvider();
-
-  PrefernecesStorage _prefsStorage = PrefernecesStorage();
-
-  factory AddressProvider.instance() => AddressProvider();
+  AddressProvider.instance();
 
   Future<List<Address>> load() async {
     // throw Exception('an error!');
-    return _prefsStorage.loadAddressList();
+    return await _repositry.loadAddressList();
   }
 
   Future<bool> save(Address address) async {
-    _prefsStorage.saveAddresses(address).then((_) {
+    await _repositry.addAddress(address).then((_) {
       return true;
     }).catchError((_) {
       return false;

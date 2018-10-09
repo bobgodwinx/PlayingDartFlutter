e69b8ec3e3f3b6120d55bb8f1f2e8daec003dcf8
Suren Grigoryan <suren.grigoryan@gmail.com>
2018-10-09 15:22:10 +0200

    Implement crash reporting to ``Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,73 @@
-import 'package:flutter/material.dart';
-import 'package:redux/redux.dart';
-import 'package:flutter_redux/flutter_redux.dart';
+import 'dart:async';
 
-import 'home/home_screen.dart';
-import 'app_state.dart';
-import 'reducers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_crashlytics/flutter_crashlytics.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
 import 'actions.dart';
 import 'add_address/add_address_screen.dart';
-import 'routes.dart';
-import 'middleware_manager.dart';
 import 'address_controller.dart';
-import 'screen.dart';
+import 'app_state.dart';
+import 'home/home_screen.dart';
+import 'middleware_manager.dart';
 import 'providers/address_provider.dart';
+import 'reducers.dart';
 import 'repositories/firebase_repository.dart';
+import 'routes.dart';
+import 'screen.dart';
 
 //Main App entrance
 void main() {
+  bool isInDebugMode = false;
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (isInDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  runZoned<Future<Null>>(_runApp, onError: _onError);
+}
+
+Future<Null> _runApp() async {
   AddressBookApp addressBookApp = AddressBookApp();
-  return runApp(addressBookApp);
+  runApp(addressBookApp);
+}
+
+void _onError(error, stackTrace) async {
+  await FlutterCrashlytics().reportCrash(error, stackTrace, forceCrash: false);
 }
 
 class AddressBookApp extends StatelessWidget {
   final Store<AppState> store = Store(
     ReducerManager().appStateReducer,
     initialState: AppState.initialState(),
-    middleware: MiddlewareManager(AddressController(AddressProvider(FirebaseRepository()))).middlewares(),
+    middleware:
+        MiddlewareManager(AddressController(AddressProvider(FirebaseRepository()))).middlewares(),
     distinct: true,
   );
 
   @override
   Widget build(BuildContext context) {
     Widget home = StoreBuilder<AppState>(
-        onInit: (Store<AppState> store) {
-          store.dispatch(ScreenUpdateAction(screen: Screen.home));
-          store.dispatch(LoadAddressesAction());
-        },
-        builder: (context, store) => HomeScreen());
+      onInit: (Store<AppState> store) {
+        store.dispatch(ScreenUpdateAction(screen: Screen.home));
+        store.dispatch(LoadAddressesAction());
+      },
+      builder: (context, store) => HomeScreen(),
+    );
 
     Widget addAddress = StoreBuilder<AppState>(
-        onInit: (Store<AppState> store) {},
-        builder: (context, store) => AddAddressScreen());
+      onInit: (Store<AppState> store) {},
+      builder: (context, store) => AddAddressScreen(),
+    );
 
     var routes = {
       Routes.home: (context) => home,
-      Routes.addAddress: (context) => addAddress
+      Routes.addAddress: (context) => addAddress,
     };
 
     MaterialApp child = MaterialApp(title: 'Hello', routes: routes);

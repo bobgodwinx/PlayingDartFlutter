5c8301aa94f291bfd7680fe7e0b84bb42943b8e6
Suren Grigoryan <suren.grigoryan@gmail.com>
2018-10-22 17:21:32 +0200

    Introduced new `navigatorKey` to be used for navigation from middlewares

--- lib/keys.dart
+++ lib/keys.dart
@@ -3,4 +3,6 @@ import 'package:flutter/widgets.dart';
 class AppKey {
   static final home = const Key('__home__');
   static final add = const Key('__add__');
+  static final navigatorKey =  GlobalKey<NavigatorState>();
+
 }
